4eda426df352c93867d14f808663465f37ac9839
Raiyan109 <kraiyan109@gmail.com>
2024-06-05 16:45:31 +0600

    database connected

---
+++ app/config/index.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv')
+const path = require('path')
+
+dotenv.config({ path: path.join((process.cwd(), ".env")) });
+
+module.exports = {
+    port: process.env.PORT,
+    database_url: process.env.MONGO_URI,
+    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+};
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors');
+const config = require('./app/config');
+
 require('dotenv').config()
 
 const app = express()
@@ -9,13 +11,13 @@ app.use(express.json())
 app.use(cors())
 
 
-// mongoose.set("strictQuery", false);
-// mongoose.connect(process.env.MONGO_URI)
-//     .then(() => {
-app.listen(process.env.PORT, (req, res) => {
-    console.log(`Server listening on ${process.env.PORT}`)
-})
-    // })
-    // .catch((error) => {
-    //     console.log(error)
-    // })
+mongoose.set("strictQuery", false);
+mongoose.connect(config.database_url)
+    .then(() => {
+        app.listen(config.port, (req, res) => {
+            console.log(`Experiment Server listening on ${config.port}`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
